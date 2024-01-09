b148c0ee27c73aa14685deb9b1a81ef459ead669
Jack Lingle <captainjack0818@gmail.com>
2024-01-09 18:39:01 -0500

    add speeds var in arcade drive with kinematics conversion

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -2,8 +2,11 @@ package frc.robot.subsystems.drive;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -11,9 +14,10 @@ public class Drive extends SubsystemBase {
     private DifferentialDriveKinematics kinematics;
     // TODO: Odometry -> add auto log with advantage kit and inputs class
     private DifferentialDriveOdometry odometry;
+
     public Drive (DriveIO io) {
         kinematics = new DifferentialDriveKinematics(Constants.DriveConstants.TRACK_WIDTH);
-        odometry = new DifferentialDriveOdometry(new Rotation2d(), 0, 0);
+        odometry = new DifferentialDriveOdometry(new Rotation2d(), 0.0, 0.0);
     }
 
     public Pose2d getPose () {
@@ -22,6 +26,12 @@ public class Drive extends SubsystemBase {
     
     public void setPose (Pose2d newPose) {
         odometry.resetPosition(new Rotation2d(), 0, 0, newPose);
+    }
+
+    public void arcadeDrive (double xSpeed, double rotation) {
+        var speeds = kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rotation));
+        // TODO: Add feedforward and PID Controller before settting voltages or add in IO class
+        // io.setVoltage (speeds.right * 12.0, speeds.left * 12.0)
     }
 
     @Override
