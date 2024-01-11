20aaed4165f83bba332b6d643b56d17f68d6ae3f
Jack Lingle <captainjack0818@gmail.com>
2024-01-10 19:12:53 -0500

    add basic Vision class

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,8 +1,21 @@
 package frc.robot.subsystems.vision;
 
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotState;
 
 public class Vision {
+    public CameraIO camera;
     public Vision () {
-    
+        if(RobotBase.isReal()) {
+            camera = new CameraIOReal()
+        } // else in sim
+    }
+
+    public void periodic () {
+        var result = camera.getResult();
+
+        if(result.hasTargets()) {
+            // change rotationSpeed (send to Drive)
+        }
     }
 }
