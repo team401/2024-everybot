7f4184f3f00c12e4190d74a71de9f93997030f7e
Jack Lingle <jacklingle@Jacks-MacBook-Pro.local>
2024-01-13 11:19:32 -0500

    create simulaed field in vision simulation class

--- src/main/java/frc/robot/subsystems/vision/VisionSimulation.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSimulation.java
@@ -1,5 +1,30 @@
 package frc.robot.subsystems.vision;
 
+import java.io.IOException;
+
+import org.photonvision.estimation.TargetModel;
+import org.photonvision.simulation.VisionSystemSim;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import frc.robot.Constants;
+
 public class VisionSimulation {
-    
+    private VisionSystemSim sim;
+    private TargetModel targetModel;
+    private AprilTagFieldLayout tagLayout;
+
+    public VisionSimulation () {
+        // create simulated field
+        sim = new VisionSystemSim(Constants.VisionConstants.CAMERA_NAME);
+        targetModel = TargetModel.kAprilTag36h11;
+        try {
+            tagLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+        } catch (IOException e) {
+            System.out.println(e); // quit program ?
+        }
+        sim.addAprilTags(tagLayout);
+
+        // create simulated camera
+    }
 }
