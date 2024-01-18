6113981bcc3a5169cb270a9120f53dad5c53ef4e
Jack Lingle <captainjack0818@gmail.com>
2024-01-18 17:18:25 -0500

    Integrate vision/navigation into robot container (#23)
    * integrate navigation into robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,20 +6,30 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.subsystems.Navigation;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  //init drive...
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
+  // make suppliers for navigation
+  // TODO: replace empty returns by integrating with relevant drive subsystem methods ( waiting on drive class )
+  private DoubleSupplier leftDistanceSupplier = () -> { return 0.0; };
+  private DoubleSupplier rightDistanceSupplier = () -> { return 0.0; };
+  private Supplier<Rotation2d> gyroSupplier = () -> { return new Rotation2d(); };
+  private Supplier<Pose2d> simulatedPoseSupplier = () -> { return new Pose2d(); };
+
+  // init navigation
+  private final Navigation nav = new Navigation(leftDistanceSupplier, rightDistanceSupplier, gyroSupplier, simulatedPoseSupplier);
+
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
