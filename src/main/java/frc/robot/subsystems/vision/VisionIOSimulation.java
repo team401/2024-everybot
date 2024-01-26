c99afc61260102cf03aad2f1da814363394e66fb
Jack Lingle <captainjack0818@gmail.com>
2024-01-26 10:55:39 -0500

    Improve pose estimation (#31)

--- src/main/java/frc/robot/subsystems/vision/VisionIOSimulation.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOSimulation.java
@@ -1,20 +1,5 @@
 package frc.robot.subsystems.vision;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.simulation.PhotonCameraSim;
-import org.photonvision.simulation.SimCameraProperties;
-import org.photonvision.simulation.VisionSystemSim;
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
-import org.photonvision.targeting.TargetCorner;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -22,8 +7,16 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.Constants;
+import java.io.IOException;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.simulation.PhotonCameraSim;
+import org.photonvision.simulation.SimCameraProperties;
+import org.photonvision.simulation.VisionSystemSim;
+import org.photonvision.targeting.PhotonPipelineResult;
 
-public class VisionIOSimulation implements VisionIO{
+public class VisionIOSimulation implements VisionIO {
     public PhotonCamera camera;
     private PhotonCameraSim simulatedCamera;
     private VisionSystemSim sim;
@@ -31,66 +24,77 @@ public class VisionIOSimulation implements VisionIO{
     private AprilTagFieldLayout layout;
     private double lastEstTimestamp = 0;
 
-    public VisionIOSimulation () {
+    public VisionIOSimulation() {
         // create simulated field
         sim = new VisionSystemSim(Constants.VisionConstants.CAMERA_NAME);
         try {
-            layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+            layout =
+                    AprilTagFieldLayout.loadFromResource(
+                            AprilTagFields.k2024Crescendo.m_resourceFile);
         } catch (IOException e) {
             System.out.println(e); // quit program ?
         }
         sim.addAprilTags(layout);
-        
+
         // set up camera
         camera = new PhotonCamera(Constants.VisionConstants.CAMERA_NAME);
-        SimCameraProperties properties =  new SimCameraProperties();
+        SimCameraProperties properties = new SimCameraProperties();
         properties.setCalibration(
-            Constants.VisionConstants.RESOLUTION_WIDTH, 
-            Constants.VisionConstants.RESOLUTION_HEIGHT, 
-            Rotation2d.fromDegrees(Constants.VisionConstants.CAM_DIAG_FOV)
-        );
+                Constants.VisionConstants.RESOLUTION_WIDTH,
+                Constants.VisionConstants.RESOLUTION_HEIGHT,
+                Rotation2d.fromDegrees(Constants.VisionConstants.CAM_DIAG_FOV));
         properties.setFPS(Constants.VisionConstants.CAMERA_FPS);
         simulatedCamera = new PhotonCameraSim(camera, properties);
-        Transform3d robotToCamera = new Transform3d(Constants.VisionConstants.BOT_TO_CAM_TRL, Constants.VisionConstants.BOT_TO_CAMERA_ROT);
+        Transform3d robotToCamera =
+                new Transform3d(
+                        Constants.VisionConstants.BOT_TO_CAM_TRL,
+                        Constants.VisionConstants.BOT_TO_CAMERA_ROT);
         sim.addCamera(simulatedCamera, robotToCamera);
 
         // set up pose estimator
-        cameraPoseEstimator = new PhotonPoseEstimator(layout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camera, robotToCamera);
+        cameraPoseEstimator =
+                new PhotonPoseEstimator(
+                        layout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camera, robotToCamera);
     }
 
-    public void updateInputs (VisionIOInputs inputs) {
+    public void updateInputs(VisionIOInputs inputs) {
         PhotonPipelineResult result = camera.getLatestResult();
         inputs.timestampSeconds = result.getTimestampSeconds();
 
         var estimate = cameraPoseEstimator.update();
-        double latestTimestamp = camera.getLatestResult().getTimestampSeconds();
+        double latestTimestamp = result.getTimestampSeconds();
         boolean newResult = Math.abs(latestTimestamp - lastEstTimestamp) > 1e-5;
         estimate.ifPresentOrElse(
-            est -> {
-                getField().getObject("VisionEstimation").setPose(est.estimatedPose.toPose2d());
-                inputs.estimatedVisionPose = est.estimatedPose.toPose2d();
-            },
-            () -> {
-                if(newResult) {
-                    getField().getObject("VisionEstimation").setPoses();
-                }
-                inputs.estimatedVisionPose = null;
-            }
-        );
-        if(newResult) {
+                est -> {
+                    if (newResult) {
+                        getField()
+                                .getObject("VisionEstimation")
+                                .setPose(est.estimatedPose.toPose2d());
+                        inputs.estimatedVisionPose = est.estimatedPose.toPose2d();
+                    } else {
+                        inputs.estimatedVisionPose = null;
+                    }
+                },
+                () -> {
+                    if (newResult) {
+                        getField().getObject("VisionEstimation").setPoses();
+                    }
+                    inputs.estimatedVisionPose = null;
+                });
+        if (newResult) {
             lastEstTimestamp = latestTimestamp;
         }
     }
 
-    public Field2d getField () {
+    public Field2d getField() {
         return sim.getDebugField();
     }
 
-    public void updatePose (Pose2d simRobotPoseMeters) {
+    public void updatePose(Pose2d simRobotPoseMeters) {
         sim.update(simRobotPoseMeters);
     }
 
-    public void set3dFieldSimActive (boolean enabled) {
+    public void set3dFieldSimActive(boolean enabled) {
         simulatedCamera.enableDrawWireframe(enabled);
     }
 }
