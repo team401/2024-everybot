4b92ff7908384f8208d51cd9cb91640905e4d18e
Bread-n-Butter06 <emilypaul@new-school.org>
2024-01-31 18:40:11 -0500

    build

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import java.util.Collections;
+
+public class RobotContainer {
+
+    CommandJoystick leftJoystick = new CommandJoystick(0);
+    CommandJoystick rightJoystick = new CommandJoystick(1);
+    CommandXboxController controller = new CommandXboxController(2);
+
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    // spotless:off
+    private void configureBindings() {
+        
+    }
+}
