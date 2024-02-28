8a39a320ffcd863973f765e10c3ac8479f0101fa
Jack Lingle <captainjack0818@gmail.com>
2024-02-28 18:46:41 -0500

    rework Interpolate double to be faster

--- src/main/java/frc/robot/subsystems/scoring/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/scoring/ShooterSubsystem.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.scoring;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.InterpolateDouble;
 
 public class ShooterSubsystem extends SubsystemBase {
     private ShooterIO io;
     private ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
+    private InterpolateDouble rpmInterpolated;
 
     public ShooterSubsystem(ShooterIO io) {
         this.io = io;
