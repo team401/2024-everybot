d2a21a2c13eae24f04da8e3e8cfd1c0e0c953555
charlesmackenzie <charlesmackenzie@mcps.org>
2024-09-18 20:08:48 -0400

    Initial setup for the swerve modules

--- src/main/java/frc/robot/constants/SwerveConstants.java
+++ src/main/java/frc/robot/constants/SwerveConstants.java
@@ -1,9 +1,9 @@
 package frc.robot.constants;
 
 public final class SwerveConstants {
-    public static final double MAX_SPEED = 5.0; // m/s
-    public static final double MAX_ANGULAR_VELOCITY = 1.0; // rad/s
+    public static final double MAX_SPEED = 0.50; // m/s
+    public static final double MAX_ANGULAR_VELOCITY = 0.1; // rad/s
 
-    public static final double MAX_OTF_SPEED = 3.0; // m/s
-    public static final double MAX_OTF_OMEGA = 0.5;
+    public static final double MAX_OTF_SPEED = 0.1; // m/s
+    public static final double MAX_OTF_OMEGA = 0.05;
 }
