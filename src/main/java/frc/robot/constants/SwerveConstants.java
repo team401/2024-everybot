a086f980ac04d0c9db43772adab6ab4d62705706
Justin Kleiber <jkleiber8@gmail.com>
2024-10-12 12:54:12 -0400

    Configuration is correct now but the controller gets stuck mid maneuver

--- src/main/java/frc/robot/constants/SwerveConstants.java
+++ src/main/java/frc/robot/constants/SwerveConstants.java
@@ -1,8 +1,8 @@
 package frc.robot.constants;
 
 public final class SwerveConstants {
-    public static final double MAX_SPEED = 0.50; // m/s
-    public static final double MAX_ANGULAR_VELOCITY = 0.1; // rad/s
+    public static final double MAX_SPEED = 2; // m/s
+    public static final double MAX_ANGULAR_VELOCITY = 2; // rad/s
 
     public static final double MAX_OTF_SPEED = 0.1; // m/s
     public static final double MAX_OTF_OMEGA = 0.05;
